baeec3dbd500d7e7964cfa33f9b5e74d4d80e9bb
LiquidIce64 <gilyazov.rm2005@gmail.com>
2025-12-29 03:58:11 +0300

    Added volume slider

--- Assets/Scripts/UI/Menu/Sliders/VolumeSlider.cs
+++ Assets/Scripts/UI/Menu/Sliders/VolumeSlider.cs
@@ -1,20 +1,15 @@
 using UnityEngine;
-using UnityEngine.Audio;
 using Utils;
 
 namespace UI
 {
     public class VolumeSlider : BaseSlider
     {
-        [SerializeField] private AudioMixer mixer;
-        [SerializeField] private AudioListener listener;
-
         protected override float GetValue() => UserSettings.Volume;
 
         protected override void SetValue(float value)
         {
-            listener.enabled = value > 0f;
-            mixer.SetFloat("Volume", Mathf.Log10(value) * 20);
+            AudioListener.volume = value;
             UserSettings.Volume = value;
             UserSettings.Save();
         }
